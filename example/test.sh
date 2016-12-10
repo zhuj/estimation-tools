c4e4483b829b723f18c0c4fd029c66f470448b3a
Viktor A. Danilov <rjabchikov.zhuj@gmail.com>
2016-12-10 23:57:30 +0300

    improvements: samples files have been updated

--- example/test.sh
+++ example/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 if true; then
+ [ -L themes ] || ln -s ../src/themes themes
+
  python2.7 ../src/estimate.py -h > estimate.help
  python2.7 ../src/estimate.py 'Estimation Tool.mm'
  python2.7 ../src/estimate.py --sort 'Estimation Tool.mm' -o 'Estimation Tool.mm.sort.xlsx'
@@ -9,6 +11,7 @@ if true; then
  python2.7 ../src/estimate.py --formulas 'Estimation Tool.mm' -o 'Estimation Tool.mm.f.xlsx'
  python2.7 ../src/estimate.py --filter-visibility --formulas --p99 'Estimation Tool.mm' -o 'Estimation Tool.mm.filter.xlsx'
  python2.7 ../src/estimate.py --theme themes.light --formulas 'Estimation Tool.mm' -o 'Estimation Tool.mm.light.xlsx'
+ python2.7 ../src/estimate.py --formulas 'Estimation Tool.deep.mm' -o 'Estimation Tool.mm.deep.xlsx'
 fi
 
 # you have to add the script from test.xba to Standard.Test module manually
